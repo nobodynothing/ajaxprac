33fbb9ce1a5df84ba99ac09a521f8d26a229027b
Justin Gordon <justinwgordon@gmail.com>
2015-08-03 15:39:38 -0700

    init

---
+++ ajaxprac.js
@@ -0,0 +1,15 @@
+function call(search) {
+  $.ajax("https://api.foursquare.com/v2/venues/search", {
+    dataType: ,
+    success: function(bluh2){
+
+    }
+  })
+}
+
+https://api.foursquare.com/v2/venues/search
+  ?client_id=CLIENT_ID
+  &client_secret=CLIENT_SECRET
+  &v=20130815
+  &ll=40.7,-74
+  &query=sushi
